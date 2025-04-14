ebbfb6e66319d8233be6d48b276010eb7d87e673
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2025-04-14 13:51:04 +0900

    docs: プロフィールと実績を更新

--- pages/index.vue
+++ pages/index.vue
@@ -5,18 +5,26 @@
     <h1 class="title is-1">Yuta Kurotaki</h1>
     <dragon/>
       <h2 class="title is-2">About</h2>
-      <p>Yuta Kurotaki is Software Engineer at <a href="https://suzuri.jp/" target="_blank">SUZURI</a> of GMO Pepabo, Inc.<br>
-      He is interested in Human Computer Interaction, Blockchain, Drums, Kickboxing.</p>
+      <p>As CTO of the SUZURI Business Division at GMO Pepabo, Inc., I oversee the technical management of "SUZURI," a platform that enables easy creation and sale of original goods and items. To create engaging experiences that spark curiosity, I drive the development and expansion of new service values utilizing AI technology. I also serve as a Developer Expert (Next-Generation IoT Systems) at GMO Internet Group, Inc.</p>
       <h2 class="title is-2">Profile</h2>
         <ul>
           <li>
-          April 2020 - Present, Ph.D. Student (Engineering), <a href="https://www.ynu.ac.jp/" target="_blank">Yokohama National University</a>
+          April 2020 - Present, Ph.D. Candidate (Engineering), <a href="https://www.ynu.ac.jp/" target="_blank">Yokohama National University</a>
           </li>
           <li>
-          July 2020 - Present, Senior Engineering Lead, <a href="https://pepabo.com/" target="_blank">GMO Pepabo, Inc.</a>
+          January 2024 - Present, CTO of SUZURI and Developer Expert, <a href="https://www.gmo.jp/" target="_blank">GMO Internet Group, Inc.</a>
           </li>
           <li>
-          April 2012 - 2018, Senior Software Engineer, <a href="https://pepabo.com/" target="_blank">GMO Pepabo, Inc.</a>
+          July 2020 - December 2023, Senior Engineering Lead, <a href="https://suzuri.jp/" target="_blank">SUZURI</a> at GMO Pepabo, Inc.
+          </li>
+          <li>
+          October 2018 - June 2020, Technical Lead, <a href="https://suzuri.jp/" target="_blank">SUZURI</a> at GMO Pepabo, Inc.
+          </li>
+          <li>
+          January 2017 - September 2018, Senior Engineer, <a href="https://pepabo.com/" target="_blank">GMO Pepabo, Inc.</a>
+          </li>
+          <li>
+          April 2012 - December 2016, Software Engineer, <a href="https://pepabo.com/" target="_blank">GMO Pepabo, Inc.</a>
           </li>
           <li>
           April 2009 - September 2011, Master of Informatics, <a href="https://www.kogakuin.ac.jp/" target="_blank">Kogakuin University</a>
@@ -27,6 +35,7 @@
         </ul>
       <h2 class="title is-2">Publications</h2>
         <ul>
+          <li><a href="https://gihyo.jp/magazine/wdpress/archive/2022/vol130" target="_blank">WEB+DB PRESS Vol.130 「作って学ぶWeb3ブロックチェーン，スマートコントラクト，NFT」特集記事寄稿</a>, 技術評論社, 2022年.</li>
           <li>東京工業大学エンジニアリングデザインプロジェクト 著, 齊藤滋規 著, 坂本啓 著, 竹田陽子 著, 角征典 著, 大内孝子 編, <a href="https://www.shoeisha.co.jp/book/detail/9784798153858" target="_blank">エンジニアのためのデザイン思考入門</a>, 翔泳社, 2017年12月15日.</li>
           <li><a href="http://gihyo.jp/magazine/wdpress/archive/2016/vol92" target="_blank">Web開発新人研修 手を動かして学ぶ開発の全体像 第4章, WEB+DB PRESS Vol.92</a>, 技術評論社, 2016年4月23日.</li>
         </ul>
@@ -42,24 +51,31 @@
         </ul>
       <h2 class="title is-2">Presentations</h2>
         <ul>
+          <li><a href="https://keebkaigi.connpass.com/event/290202/" target="_blank">KeebKaigi 2023</a></li>
+          <li><a href="https://devrel.dev/japan-2023/" target="_blank">DevRel/Japan CONFERENCE 2023</a></li>
+          <li><a href="https://ethglobal.com/events/tokyo2023" target="_blank">ETHGlobal Tokyo 2023</a></li>
+          <li><a href="https://fgn.jp/event/future-talks-2/" target="_blank">もうちょっと知りたいWeb3 〜うねりの最前から見えるトレンド〜：FGN Future Talks #2</a></li>
+          <li><a href="https://www.ipsj.or.jp/event/iot/2023/" target="_blank">情報処理学会IoT行動変容学研究グループ キックオフシンポジウム 研究奨励賞</a></li>
           <li><a href="https://speakerdeck.com/kurotaky/ethereum-for-ruby" target="_blank">Ethereum for Ruby - RubyKaigi 2022</a></li>
+          <li><a href="https://makezine.jp/event/makers-mft2022/" target="_blank">Maker Faire Tokyo 2022</a></li>
           <li><a href="https://rubykaigi.org/2018/presentations/kurotaky.html#may31" target="_blank">bancor: Token economy made with Ruby - RubyKaigi 2018</a></li>
           <li><a href="https://kurotaky.hatenablog.com/entry/2017/03/28/211836" target="_blank">CycleChanger - Slush Tokyo 2017</a></li>
           <li><a href="https://rubyassociation.doorkeeper.jp/events/36358" target="_blank">新規事業の成長を支えるRuby - Ruby Business Users Conference 2016</a></li>
           <li><a href="https://2015.rubyconf.tw/" target="_blank">Enterprise Rails Application Tips - RubyConf Taiwan 2015</a></li>
         </ul>
-      <h2 class="title is-2">Paper/International Conference</h2>
+      <h2 class="title is-2">Media</h2>
         <ul>
-          <li>
-            Yuta Kurotaki, Hisao Shiizuka, Measurment and Clasification of the Affective Value of Drum Rhythm Patterns,<br>
-            Third IEEE International Conference on Intelligent Networking and Collaborative Systems(IEEE INCoS2011), 872-875, 2011-12.<br>
-            <a href="https://ieeexplore.ieee.org/document/6132925/" target="_blank">https://ieeexplore.ieee.org/document/6132925/</a>
-          </li>
-          <li>
-            Yuta Kurotaki, Hisao Shiizuka, Evaluation of Feelings Received from the Rhythms of Percussive Timbre and Relationships between Affective Values,<br>
-            Advances in Intelligent Decision Technologies Proceedings of the Second KES International Symposium IDT 2010, Springer, 631-639, 2010-07-30.<br>
-            <a href="https://link.springer.com/chapter/10.1007/978-3-642-14616-9_62" target="_blank">https://link.springer.com/chapter/10.1007/978-3-642-14616-9_62</a>
-          </li>
+          <li><a href="https://www.ipa.go.jp/jinzai/mitou/2023/2023_mitou_target.html" target="_blank">GMOインターネットグループ所属パートナー 黒瀧 悠太が提案したプロジェクトが IPA「2023年度 未踏ターゲット事業」に採択</a></li>
+          <li><a href="https://www.gmo.jp/news/article/10119/" target="_blank">GMOペパボ、「SUZURI byGMOペパボ」にNFTコンテンツを使いオリジナルグッズを作成・販売できる機能を追加</a></li>
+          <li><a href="https://www.gmo.jp/news/article/10099/" target="_blank">「ペパボ研究所」と横浜国立大学の共同研究が、科学技術振興機構の戦略的創造研究推進事業「AIP加速課題」に採択</a></li>
+          <li><a href="https://pepabo.com/blog/2023/03/31/interview-kurotaki/" target="_blank">学び続けることで、新しい視野と価値を生み出す。ペパボ社会人学生インタビュー</a></li>
+          <li><a href="https://talent.supplyzine.com/2023/05/15/gmo-all-right-nippon/" target="_blank">技育祭2023［春］登壇レポート 「GMOのオールライトニッポン！〜若手エンジニアが現場のリアルを語ります〜」</a></li>
+        </ul>
+      <h2 class="title is-2">Other Activities</h2>
+        <ul>
+          <li>鹿児島Ruby会議01実行委員長</li>
+          <li>鹿児島Ruby会議02実行委員長</li>
+          <li>Rails Girls Tokyo Coach</li>
         </ul>
       <h2 class="title is-2">Links</h2>
         <ul>
